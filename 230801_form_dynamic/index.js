82cbbc2ddbd2d93673fecd735ee8f8316ccfcfd6
DongHo-Kang <gkqjem981113@gmail.com>
2023-08-01 13:08:28 +0900

    230801 동적 form 전송

---
+++ 230801_form_dynamic/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//ajax
+app.get("/ajax", (req, res) => {
+  console.log("back", req.query);
+  res.send(req.query);
+});
+
+app.post("/ajax", (req, res) => {
+  console.log("back", req.body);
+  res.send(req.body);
+});
+
+//axios
+app.get("/axios", (req, res) => {
+  console.log("back", req.query);
+  res.send(req.query);
+});
+
+app.post("/axios", (req, res) => {
+  console.log("back", req.body);
+  res.send(req.body);
+});
+
+//fetch
+app.get("/fetch", (req, res) => {
+  console.log("back", req.query);
+  res.send(req.query);
+});
+
+app.post("/fetch", (req, res) => {
+  console.log("back", req.body);
+  res.send(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
