757dfb172286c220b04c2e28a5e413d6b7c98d5f
DongHo-Kang <gkqjem981113@gmail.com>
2023-08-02 10:34:47 +0900

    form 전송 수정

--- 230801_form_dynamic/practice1_index.js
+++ 230801_form_dynamic/practice1_index.js
@@ -15,9 +15,13 @@ app.set("views", "./views");
 app.get("/", (req, res) => {
   res.render("practice1_index");
 });
-
+const id = 123;
+const pw = 456;
 app.post("/axios", (req, res) => {
   console.log("back", req.body);
+  if ((`${res.data.id}` == correctID) & (`${res.data.pw}` == correctPW)) {
+    res.send(1);
+  }
   res.send(req.body);
 });
 
