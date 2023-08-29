d2af39fdf53c0956a116b451dde0141a5af6a5cf
DongHo-Kang <gkqjem981113@gmail.com>
2023-08-29 09:15:20 +0900

    웹소켓에서 http와 웹소켓서버 양분화

--- 230828_webSocket_class/index.js
+++ 230828_webSocket_class/index.js
@@ -1,16 +1,15 @@
-const ws = require('ws');
-const express = require('express');
+const http = require("http");
+const ws = require("ws");
+const express = require("express");
 const app = express();
+const server = http.createServer(app); //http 서버를 연다.
+
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('client');
-});
-
-const server = app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.render("client");
 });
 
 //웹소켓 서버 접속
@@ -18,28 +17,32 @@ const wss = new ws.Server({ server });
 //브라우저(클라이언트)들을 담을 변수선언
 const sockets = [];
 //socket변수는 접속한 브라우저
-wss.on('connection', (socket) => {
-    console.log('클라이언트가 연결되었습니다');
-    //sockets배열에 브라우저 추가
-    sockets.push(socket);
+wss.on("connection", (socket) => {
+  console.log("클라이언트가 연결되었습니다");
+  //sockets배열에 브라우저 추가
+  sockets.push(socket);
 
-    socket.on('message', (message) => {
-        console.log(message);
-        //msg: {user: "any", message: "any"}
-        const msg = JSON.parse(message);
-        console.log(`클라이언트로부터 받은 메세지: ${msg.message}`);
-        //클라이언트로 응답 메세지 전송
-        //socket.send(`서버메세지: ${message}`);
-        sockets.forEach((elem) => {
-            elem.send(`${msg.user}: ${msg.message}`);
-        });
+  socket.on("message", (message) => {
+    console.log(message);
+    //msg: {user: "any", message: "any"}
+    const msg = JSON.parse(message);
+    console.log(`클라이언트로부터 받은 메세지: ${msg.message}`);
+    //클라이언트로 응답 메세지 전송
+    //socket.send(`서버메세지: ${message}`);
+    sockets.forEach((elem) => {
+      elem.send(`${msg.user}: ${msg.message}`);
     });
-    //오류
-    socket.on('error', (err) => {
-        console.log('에러가발생하였습니다', err);
-    });
-    //접속종료
-    socket.on('close', () => {
-        console.log('클라이언트와 연결이 종료됨');
-    });
+  });
+  //오류
+  socket.on("error", (err) => {
+    console.log("에러가발생하였습니다", err);
+  });
+  //접속종료
+  socket.on("close", () => {
+    console.log("클라이언트와 연결이 종료됨");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
