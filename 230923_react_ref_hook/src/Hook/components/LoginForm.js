63441a0b748d6392bcccc92d017930fffa7fa230
DongHo-Kang <gkqjem981113@gmail.com>
2023-09-25 18:52:51 +0900

    230923 수정

--- 230923_react_ref_hook/src/Hook/components/LoginForm.js
+++ 230923_react_ref_hook/src/Hook/components/LoginForm.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function LoginForm({ state, dispatch }) {
-  const [id, setID] = useState("");
-  const [pw, setPW] = useState("");
-  const userInfo = { realId: "banana", realPw: "4321" };
+  const [id, setID] = useState('');
+  const [pw, SetPW] = useState('');
+  const userInfo = { realId: 'banana', realPw: '4321' };
 
   const handleLoginForm = (event) => {
     event.preventDefault();
-    const [realId, realPw] = userInfo;
-    if (id === realId && pw === realPw) dispatch({ type: "LOGIN_SUCCESS" });
-    else if (id !== realId && pw === realPw) dispatch({ type: "MISS_ID" });
-    else if (id === realId && pw !== realPw) dispatch({ type: "MISS_PW" });
+    const { realId, realPw } = userInfo;
+
+    if (id === realId && pw === realPw) dispatch({ type: 'LOGIN_SUCCESS' });
+    else if (id !== realId && pw === realPw) dispatch({ type: 'MISS_ID' });
+    else if (id === realId && pw !== realPw) dispatch({ type: 'MISS_PW' });
+    else dispatch({ type: 'LOGIN_FAILURE' });
   };
 
   return (
@@ -20,7 +22,7 @@ export default function LoginForm({ state, dispatch }) {
       <br />
 
       <label htmlFor="pw">PW</label>
-      <input id="pw" type="password" onChange={(e) => setPW(e.target.value)} />
+      <input id="pw" type="password" onChange={(e) => SetPW(e.target.value)} />
       <br />
 
       <button>Login</button>
--- 230923_react_ref_hook/src/reducer/Reducer.js
+++ 230923_react_ref_hook/src/reducer/Reducer.js
@@ -1,38 +1,38 @@
-export default function Reducer(state, action){
-    switch(action.type){
-        case "LOGIN_SUCCESS";
-        return{
-            ...state,
-            isLogin: true,
-            message: "로그인 성공"
-        };
-        case "MISS_ID ":
-            return{
-                ...state,
-                isLogin: false,
-                message: "아이디 오류"
-            }; 
-        case "MISS_PW ":
-                return{
-                    ...state,
-                    isLogin: false,
-                    message: "비밀번호 오류"
-                
-                }; 
-                case "LOGIN_FAILURE ":
-                    return{
-                        ...state,
-                        isLogin: false,
-                        message: "아이디랑 비밀번호 모두 오류"
-                    
-                    }; 
-                    case "LOGOUT":
-                        return{
-                            ...state,
-                            isLogin: false,
-                            message: "로그아웃!"
-                        
-                        }; 
-                        default: return 
-    }
+export default function Reducer(state, action) {
+  switch (action.type) {
+    case 'LOGIN_SUCCESS':
+      return {
+        ...state,
+        isLogin: true,
+        message: '로그인 성공!',
+      };
+    case 'MISS_ID':
+      return {
+        ...state,
+        isLogin: false,
+        message: '아이디 오류!',
+      };
+    case 'MISS_PW':
+      return {
+        ...state,
+        isLogin: false,
+        message: '비밀번호 오류!',
+      };
+    case 'LOGIN_FAILURE':
+      return {
+        ...state,
+        isLogin: false,
+        message: '아이디랑 비밀번호 모두 오류!',
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        isLogin: false,
+        message: '로그아웃!',
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
 }
