45c7840f4a817941f0169c77c76d4d318be908b0
DongHo-Kang <gkqjem981113@gmail.com>
2023-10-20 23:24:54 +0900

    JAVA 기본 데이터 타입

---
+++ KDT/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요");
+		System.out.printf("%d * %d = %d", 10,2,10*2).println();
+		//형식 지정자
+		//%d : 정수
+		//%f : 실수 // %.2f : 소수점 둘째자리까지
+		//%s : 문자열
+		//%c : 문자
+		//%b : boolean
+	}
+
+}
+
