f5c27742eb03742234cb4e2643a2c61f8c754afc
DongHo-Kang <gkqjem981113@gmail.com>
2023-10-21 12:13:10 +0900

    배열 기초

---
+++ KDT/src/Java_Basic/Array_231021.java
@@ -0,0 +1,42 @@
+package Java_Basic;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_231021 {
+
+	public static void main(String[] args) {
+		//배열 선언
+		int[] intArr;
+		char[] charArr;
+		
+		//배열 생성
+		//배열 생성하면 크기 변경이 안된다.
+		int[] intArr1 = new int[5];
+		
+		//배열 초기화
+		int[] intArr2 = {1,2,3,4,5};
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i < intArr1.length; i++) {
+			intArr1[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(intArr1));
+		sc.close();
+		
+		//2차원 배열
+		int[][] intDoubleArr;
+		char[][] charDoubleArr;
+		
+		int[][] intDoubleArr2 = new int[2][3];
+		
+		int intDoubleArr3[][] = {{0,1,2},{3,4,5}};
+		
+		//for-each문
+		for(int value: intArr1) {
+			System.out.print(value + " ");
+		}
+		
+	}
+
+}
